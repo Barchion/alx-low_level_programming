e664439eeb2bd494fd06009bb176f6a1e61474c0
Chidera Onyeka <barchion@gmail.com>
2022-09-08 21:09:59 -0700

    Cool, updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main- Serves as the entry poin for the program
  *
- * Return: By default returns zero if no errors otherwise returns non-zero value.
+ * Return. Always 0
  */
 int main(void)
 {
