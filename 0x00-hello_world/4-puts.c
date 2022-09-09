f1cff11c12c884b971415b8473559e834531d10d
Chidera Onyeka <barchion@gmail.com>
2022-09-08 20:52:20 -0700

    Updated now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingualpuzzle");
-	return(0)
+	puts("\"Programming is like building a multilingualpuzzle puzzle");
+	return(0);
 }
 
