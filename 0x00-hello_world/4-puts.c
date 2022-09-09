db8e77bc302fd4825685cc55a5c5f94765a503b5
Chidera Onyeka <barchion@gmail.com>
2022-09-08 20:46:02 -0700

    E no easy

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry poin for the program
+ *
+ * Return: By default returns zero if no errors otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingualpuzzle");
+	return(0)
+}
+
